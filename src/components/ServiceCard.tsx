28607574de118783870e3784b0d28079e69cd76a
HammadGhuman <k201720@nu.edu.pk>
2023-05-20 12:47:03 +0500

    phase1

--- src/components/ServiceCard.tsx
+++ src/components/ServiceCard.tsx
@@ -1,23 +1,36 @@
 "use client";
-import React from 'react'
-import Image from 'next/image'
-import bg from '../../public/Serviceblob.svg'
+import React from "react";
+import Image from "next/image";
+import bg from "../../public/Serviceblob.svg";
+import TechStackButton from "./TechStackButton";
+import Lottie from 'lottie-react';
 type Props = {
-    cardImg:any
-    text:any
-}
+  AnimationData:any
+  heading: any;
+  text: any;
+  color:any;
+};
 
-const ServiceCard = ({cardImg,text}: Props) => {
+const ServiceCard = ({ AnimationData, heading,text,color }: Props) => {
+  console.log(`bg-${color}`)
   return (
-    <div className='relative px-12 pb-44 flex flex-row justify-between rounded-br-2xl rounded-bl-2xl  text-white font-medium'>
-            <div className='max-w-[50%] text-xl font-semibold flex items-center justify-between' >
-                <p>{text}</p> 
-            </div>
-            <div className='max-w-[50%]'>
-                <Image src={cardImg} alt="webdev png" className='w-[250px] h-[250px]'/>
-            </div>
-    </div>
-  )
-}
 
-export default ServiceCard
+    <div className={`flex flex-col lg:flex-row justify-evenly lg:mx-28 my-5 py-10 ${color} hover:bg-gradient-to-tr hover:from-primary hover:to-[#4400a6] transition-all duration-300 rounded-lg`}>
+      <div className="flex flex-col space-y-10 pl-10 text-white">
+        <h1 className="text-4xl">{heading}</h1>
+        <p className="max-w-lg">
+    {text}
+        </p>
+        <div className="grid grid-flow-row grid-cols-3 max-w-sm gap-3">
+          <TechStackButton />
+          <TechStackButton />
+          <TechStackButton />
+          <TechStackButton />
+        </div>
+      </div>
+        <Lottie className="max-w-[300px] max-h-[300px] self-center" loop={false}  animationData={AnimationData}/>
+    </div>
+  );
+};
+
+export default ServiceCard;
--- src/components/Story.tsx
+++ src/components/Story.tsx
@@ -1,11 +1,86 @@
-import React from 'react'
+import React from "react";
 
-type Props = {}
+type Props = {};
 
 const Story = (props: Props) => {
   return (
-    <div>Story</div>
-  )
-}
+    <div className="mt-5 px-[10vw] pb-16">
+      <h1 className="text-center mt-16 text-5xl font-bold">About Us</h1>
+      <h1 className="text-3xl mt-16">We are a software development team with a <span className="text-primary"> major twist! </span></h1>
+      <p className="text-xl text-gray-500 mt-6">At CodeMate, we are passionate about creating exceptional software solutions while delivering outstanding customer service. We understand that our clients' success relies not only on the quality of our software but also on the level of support and satisfaction they experience throughout their journey with us.</p>
+      <p className="text-xl text-gray-500 mt-6">The very foundation of CodeMate has been laid on delivering top-notch customer service and ensuring client  satisfaction every step of the way.</p>
+      <div className="grid grid-cols-2 grid-rows-3 mt-16">
+      <div>
+        <h1 className="text-3xl mb-8" ><span className="text-primary">Customer-Centric</span> Approach</h1>
+        <p className="text-xl w-[90%] text-gray-500">
+          At the heart of our company is a deep commitment to our clients. We
+          firmly believe that our success is intertwined with their success.
+          That&apos;s why we have made customer service and satisfaction our top
+          priorities. We go above and beyond to understand our clients&apos; needs,
+          goals, and pain points, ensuring that we develop tailored solutions
+          that address their specific requirements.
+        </p>
+      </div>
+      <div>
+      <h1 className="text-3xl mb-8" ><span className="text-primary">Experienced and Dedicated </span> Team</h1>
+      <p className="text-xl w-[90%] text-gray-500">
+          Our team comprises highly skilled and experienced professionals who
+          are dedicated to delivering excellence. From our software developers
+          and engineers to our project managers and support staff, every member
+          of our team is committed to providing exceptional service. We
+          continuously invest in their growth and development, keeping them
+          updated with the latest industry trends and technologies, so we can
+          consistently deliver cutting-edge solutions to our clients.
+        </p>
+      </div>
+      
+      <div>
+        <h1 className="text-3xl mb-8 text-primary">Quality Assurance and Testing</h1>
+        <p className="text-xl text-gray-500 w-[90%]">
+          To ensure the highest quality of our software solutions, we follow
+          rigorous testing and quality assurance processes. We conduct
+          comprehensive testing at every stage of development to identify and
+          resolve any issues or bugs promptly. Our commitment to quality means
+          that our clients can rely on our software to perform flawlessly,
+          contributing to their overall satisfaction and success.
+        </p>
+      </div>
+      <div>
+        <h1 className="text-3xl mb-8 text-primary">Collaboration and Communication</h1>
+        <p className="text-xl text-gray-500 w-[90%]">
+          We believe that open and transparent communication is essential for
+          successful collaborations. We work closely with our clients, keeping
+          them involved and informed throughout the entire development process.
+          We listen to their feedback, suggestions, and concerns, ensuring that
+          we are aligned with their vision and goals. This collaborative
+          approach allows us to build strong, long-lasting partnerships with our
+          clients.
+        </p>
+      </div>
+      <div>
+        <h1 className="text-3xl mb-8 text-primary">Continuous Improvement</h1>
+        <p className="text-gray-500 w-[90%] text-xl">
+          We understand that technology is constantly evolving, and we strive to
+          stay ahead of the curve. We embrace a culture of continuous
+          improvement, always seeking opportunities to enhance our processes,
+          technologies, and customer service. By staying at the forefront of the
+          industry, we can provide our clients with the most innovative and
+          efficient solutions available. At CodeMate, we are committed to
+          building long-term relationships based on trust, transparency, and
+          outstanding customer service. Partner with us, and experience the
+          difference of working with a software development company that truly
+          cares about your success.
+        </p>
+      </div>
+    
+      </div>
+     
+    
+      
+      
+    </div>
+    
+  );
+};
 
-export default Story
+export default Story;
